f5729c73a9a010fd0f58267ecd931d8f9d5cc481
quandnguyen <quan@levelmoney.com>
2016-10-16 21:54:10 -0700

    week 4 homework

--- Week 4 - Tumblr/Week 4 - Tumblr/ComposeViewController.swift
+++ Week 4 - Tumblr/Week 4 - Tumblr/ComposeViewController.swift
@@ -16,51 +16,97 @@ class ComposeViewController: UIViewController {
     @IBOutlet weak var chatButton: UIButton!
     @IBOutlet weak var videoButton: UIButton!
     
-    var composeButtons: [UIButton]!
+    //make an array to hold the buttons
+    var buttons: [UIButton]!
     
-    var composeButtonsOriginalCenter: CGPoint!
-    var composeButtonsOffset: CGFloat!
-    var composeButtonsCenter: CGPoint!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        //put the buttons in the defined array
+        buttons = [textButton, photoButton, quoteButton, linkButton, chatButton, videoButton]
+        
     }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        composeButtonsOffset = 100
-//        composeButtonsCenter = composeButtons.center
-//        
-//        UIView.animate(withDuration: 0.5, delay: 0, options: [.repeat, .autoreverse],
-//                       animations: {
-//                        self.composeButtons.center = self.frame.size.heigth + composeButtonsOffset
-//                    
-//            }, completion: nil)
-//
-//        
-//    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        for i in buttons {
+            i.center.y = view.frame.size.height
+            i.alpha = 0
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: [], animations: {
+            self.textButton.center.y -= self.view.frame.size.height/1.6
+            self.textButton.alpha = 1
+            }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.2, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: [], animations: {
+            self.photoButton.center.y -= self.view.frame.size.height/1.6
+            self.photoButton.alpha = 1
+            }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.3, usingSpringWithDamping: 0.8, initialSpringVelocity: 2, options: [], animations: {
+            self.quoteButton.center.y -= self.view.frame.size.height/1.6
+            self.quoteButton.alpha = 1
+            }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.5, usingSpringWithDamping: 1, initialSpringVelocity: 3, options: [], animations: {
+            self.linkButton.center.y -= self.view.frame.size.height/2.5
+            self.linkButton.alpha = 1
+            }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.6, usingSpringWithDamping: 0.8, initialSpringVelocity: 2, options: [], animations: {
+            self.chatButton.center.y -= self.view.frame.size.height/2.5
+            self.chatButton.alpha = 1
+            }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.8, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [], animations: {
+            self.videoButton.center.y -= self.view.frame.size.height/2.5
+            self.videoButton.alpha = 1
+            }, completion: nil)
+
+        
+    }
+    
     
     @IBAction func didTapDismiss(_ sender: UIButton) {
-        dismiss(animated: true, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.25, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: [], animations: {
+            self.textButton.center.y += self.view.frame.size.height
+            self.textButton.alpha = 0
+            }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.2, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: [], animations: {
+            self.photoButton.center.y += self.view.frame.size.height
+            self.photoButton.alpha = 0
+            }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.15, usingSpringWithDamping: 0.8, initialSpringVelocity: 2, options: [], animations: {
+            self.quoteButton.center.y += self.view.frame.size.height
+            self.quoteButton.alpha = 0
+            }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.1, usingSpringWithDamping: 1, initialSpringVelocity: 3, options: [], animations: {
+            self.linkButton.center.y += self.view.frame.size.height
+            self.linkButton.alpha = 0
+            }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0.05, usingSpringWithDamping: 0.8, initialSpringVelocity: 2, options: [], animations: {
+            self.chatButton.center.y += self.view.frame.size.height
+            self.chatButton.alpha = 0
+            }, completion: nil)
+        
+        UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [], animations: {
+            self.videoButton.center.y += self.view.frame.size.height
+            self.videoButton.alpha = 0
+            }, completion: {finished in self.dismiss(animated: true, completion: nil)})
     }
     
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Week 4 - Tumblr/Week 4 - Tumblr/SearchViewController.swift
+++ Week 4 - Tumblr/Week 4 - Tumblr/SearchViewController.swift
@@ -9,17 +9,58 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    @IBOutlet weak var searchFeedView: UIImageView!
+    @IBOutlet weak var loadingView: UIImageView!
+    
+    var loading_1: UIImage!
+    var loading_2: UIImage!
+    var loading_3: UIImage!
+    //class for UIIMage array
+    var images: [UIImage]!
+    var animatedImage: UIImage!
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Link the actual image files in your assets to your image variables
+        loading_1 = UIImage(named: "loading-1")
+        loading_2 = UIImage(named: "loading-2")
+        loading_3 = UIImage(named: "loading-3")
+        
+        images = [loading_1, loading_2, loading_3]
+        
+        animatedImage = UIImage.animatedImage(with: images, duration: 1.0)
+        
+        
+        loadingView.image = animatedImage
+        
+
 
         // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        searchFeedView.alpha = 0
+        loadingView.alpha = 1
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2), execute: {
+            // Put your code which should be executed with a delay here
+            self.searchFeedView.alpha = 1
+            self.loadingView.alpha = 0
+        })
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
 
     /*
